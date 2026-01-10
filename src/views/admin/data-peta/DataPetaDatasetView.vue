bc6fcd653037844122b5a857bc23f2afeff28837
bagus winarno <bagus.stimata@gmail.com>
2026-01-10 13:44:40 +0700

    tambah dataset

--- src/views/admin/data-peta/DataPetaDatasetView.vue
+++ src/views/admin/data-peta/DataPetaDatasetView.vue
@@ -1,15 +1,15 @@
 <template>
-    <v-card class="mt-2 ml-2 mr-2" elevation="0">
+    <v-card elevation="0">
       <FDayaDukungPetaTable></FDayaDukungPetaTable>
     </v-card>
   </template>
-  
+
   <script>
 
   import FDayaDukungPetaTable from "@/components/admin/data-peta/daya-dukung-peta/FDayaDukungPetaTable.vue"
 
   export default {
-    name: "DayaDukungView",
+    name: "DataPetaDatasetView",
     components: {
       FDayaDukungPetaTable
     },
